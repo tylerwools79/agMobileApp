7c13792d1761595313a6ffc9b527115bc73e276d
tylerwools79 <tylerwools79@gmail.com>
2021-03-10 12:22:30 -0700

    Built the basic structure and have it set up to be stateful.

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,256 @@
+import 'package:ag_coding_project/presentation/custom_icon_icons.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() =>  runApp(MaterialApp(
+  home: Home()
+));
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _HomeState extends State<Home> {
+  String fName = '';
+  String lName = '';
+  String phoneNumber = '';
+  String eMail = '';
+  String aboutMe = '';
+  String profilePicPath='assets/missing texture.jpg';
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Edit Profile'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Edit Profile',
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 20.0,
+            letterSpacing: 1.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        //color: Colors.blue,
+        elevation: 0.0,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 30, 0, 20),
+              child: TextButton(
+                //This is the profile picture section
+                onPressed: (){
+                  setState((){ //this does not work yet!
+                    profilePicPath='assets/default.jpg';
+                    print('Change profile pic');
+                  });
+                },
+                child: new Stack(
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundColor: Colors.blue,
+                      radius: 75,
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage('assets/default.jpg'),//Image(image: FileImage(File(profilePicPath))),
+                        radius: 70,
+                    ),
+                  ),
+                    new Positioned(
+                      right: 0.0,
+                      top: 0.0,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                          child: new Icon(
+                            Icons.account_circle_rounded,
+                            color: Colors.blue,
+                          )
+                      )
+                    )
+                 ]
+                )
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            TextButton(
+                onPressed: (){
+                  setState((){
+                    fName='John';
+                    lName='Doe';
+                  });
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('NAME',
+                          style: TextStyle(
+                            color: Colors.grey[400],
+                            letterSpacing: 0.5,
+                          ),
+                          ),
+                          SizedBox(height:5.0),
+                          Text('$fName $lName',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 0.5,
+                          )
+                          ),
+                          SizedBox(height:10),
+                          Divider(height: 20),
+                        ],
+                      ),
+                    ),
+                    Icon(
+                      Icons.arrow_forward_ios_rounded,
+                      color: Colors.grey[400],
+                        size: 35.0,
+
+                    )
+                  ],
+                )
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            TextButton(
+                onPressed: (){
+                  setState((){
+                    phoneNumber = '1-(555)-555-5555';
+                  });
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('PHONE NUMBER',
+                            style: TextStyle(
+                              color: Colors.grey[400],
+                              letterSpacing: 0.5,
+                            ),
+                          ),
+                          SizedBox(height:5.0),
+                          Text('$phoneNumber',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 0.5,
+                              )
+                          ),
+                          SizedBox(height:10),
+                          Divider(height: 20),
+                        ],
+                      ),
+                    ),
+                    Icon(
+                      Icons.arrow_forward_ios_rounded,
+                      color: Colors.grey[400],
+                      size: 35.0,
+
+                    )
+                  ],
+                )
+            ),
+            TextButton(
+                onPressed: (){
+                  setState((){
+                    eMail='john.doe@email.com';
+                  });
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('EMAIL',
+                            style: TextStyle(
+                              color: Colors.grey[400],
+                              letterSpacing: 0.5,
+                            ),
+                          ),
+                          SizedBox(height:5.0),
+                          Text('$eMail',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 0.5,
+                              )
+                          ),
+                          SizedBox(height:10),
+                          Divider(height: 20),
+                        ],
+                      ),
+                    ),
+                    Icon(
+                      Icons.arrow_forward_ios_rounded,
+                      color: Colors.grey[400],
+                      size: 35.0,
+
+                    )
+                  ],
+                )
+            ),
+            TextButton(
+                onPressed: (){
+                  setState((){
+                    aboutMe='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dolor diam, lobortis in semper et, facilisis at metus. Fusce iaculis scelerisque nibh et aliquet. Fusce iaculis ligula et lectus viverra sollicitudin. Nulla velit arcu, imperdiet id molestie eu, mollis eget metus. Mauris nisl mauris, dapibus vitae risus ac, fringilla varius.';
+                  });
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('TELL US ABOUT YOURSELF',
+                            style: TextStyle(
+                              color: Colors.grey[400],
+                              letterSpacing: 0.5,
+                            ),
+                          ),
+                          SizedBox(height:5.0),
+                          ConstrainedBox(
+                            constraints: BoxConstraints(maxHeight: 50),
+                            child: Text('$aboutMe',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 0.5,
+                                )
+                            ),
+                          ),
+                          SizedBox(height:10),
+                          Divider(height: 20),
+                        ],
+                      ),
+                    ),
+                    Icon(
+                      Icons.arrow_forward_ios_rounded,
+                      color: Colors.grey[400],
+                      size: 35.0,
+
+                    )
+                  ],
+                )
+            ),
+          ]
+        )
+
+      )
     );
   }
 }
